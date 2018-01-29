a29c3d172ce1fc437fe0d5351b2d37942366b60f
K. Kartari <kkartari7@gmail.com>
2018-01-29 10:38:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-describe('Beatles Loops', function() {
-  it("returns an array of strings containing what instruments each musician plays", function() {
-    const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-    const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-    expect(theBeatlesPlay(musicians, instruments)).toEqual(["John Lennon plays Guitar", "Paul McCartney plays Bass Guitar", "George Harrison plays Lead Guitar", "Ringo Starr plays Drums"]);
-  });
-
   function theBeatlesPlay(musicians, instruments) {
     var arrayA = []
     for (var indy = 0 < musicians.length; indy++) {
