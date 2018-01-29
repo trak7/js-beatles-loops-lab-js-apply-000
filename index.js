572a3699c3a609b5a7be2bb02c6ee9a2e3a5973a
K. Kartari <kkartari7@gmail.com>
2018-01-29 10:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-  function theBeatlesPlay(musicians, instruments) {
-    var arrayA = []
-    for (var indy = 0 < musicians.length; indy+) {
-      allMusicians.push(musicians[indy] + "plays" + instruments[indy])
-    }
-    return allMusicians
-  }
+function theBeatlesPlay (musicians, instruments) {
+var newArray = []
+for (var i = 0; i < musicians.length; i++) {
+newArray.push(musicians[i] + " plays " + instruments[i]);
+}
+return newArray;
+}
